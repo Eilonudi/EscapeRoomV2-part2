d663ce01488f7f8cf7bfbd8fa2613d961f1e7460
Eilonudi <eilonudi@gmail.com>
2023-02-20 00:40:27 +0200

    Adding solution code

--- Assets/Scenes/Escape Room/Scripts/TvPuzzle.cs
+++ Assets/Scenes/Escape Room/Scripts/TvPuzzle.cs
@@ -1,51 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class TvPuzzle : MonoBehaviour
 {
+    [Range(1000, 9999)]
+    public int puzzleChannel = 5655;
+    
+    [Range(1000, 9999)]
+    public int teacherCode = 4444;
+    
     public TextMeshProUGUI channelText;
+    public TextMeshProUGUI puzzleAnswerText;
     
     private readonly List<int> _enteredNumbers = new List<int>();
+    private bool _solved = false;
+    private VideoPlayer _tvVideo;
     
     void Start()
     {
-        // clear channel text
+        // clear channel and answer text
         channelText.text = "";
-        SetChannelNumber(1);
-        SetChannelNumber(3);
+        puzzleAnswerText.text = "";
+        _tvVideo = GetComponentInChildren<VideoPlayer>();
     }
-
     
-    void Update()
+    public void SetChannelNumber(int enteredNum)
     {
+        if (!_solved)
+        {
+            _enteredNumbers.Add(enteredNum);
+            var numbersString = "";
+        
+            for (int j = 4 -_enteredNumbers.Count; j > 0; j--)
+            {
+                numbersString += "_ ";
+            }
+        
+            for (int i = 0; i < _enteredNumbers.Count; i++)
+            {
+                numbersString += _enteredNumbers[i].ToString() + " ";
+            }
+            channelText.text = numbersString;
+
+            CheckIfSolved(numbersString);
+        }
         
     }
 
-    void SetChannelNumber(int enteredNum)
+    private void CheckIfSolved(string numbersString)
     {
-        _enteredNumbers.Add(enteredNum);
-        // only 3 numbers are allowed
-        if (_enteredNumbers.Count > 3)
+        if (_enteredNumbers.Count == 4)
         {
-            _enteredNumbers.Clear();
-        };
-        
-        var numbersString = "";
-        
-        for (int j = 3 -_enteredNumbers.Count; j > 0; j--)
-        {
-            numbersString += "_ ";
+            var clearedNumberString = Regex.Replace(numbersString, @"\s+", "");
+            var channel = Int32.Parse(clearedNumberString);
+            if (channel == puzzleChannel)
+            {
+                _solved = true;
+                ShowSolution();
+            }
+            else
+            {
+                channelText.text = "";
+                _enteredNumbers.Clear();
+            }
         }
-        
-        for (int i = 0; i < _enteredNumbers.Count; i++)
-        {
-            numbersString += _enteredNumbers[i].ToString() + " ";
-        }
-
-        
-
-        channelText.text = numbersString;
+    }
+    
+    private void ShowSolution()
+    {
+        channelText.text = "";
+        _tvVideo.Stop();
+        puzzleAnswerText.text = "Course code is \n " + teacherCode;
     }
 }
