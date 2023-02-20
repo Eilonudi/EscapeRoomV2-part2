e59e10d8874f13d34f3c6511a85a4cb410b5b328
Eilonudi <eilonudi@gmail.com>
2023-02-21 00:46:08 +0200

    adding script for trash puzzle

---
+++ Assets/Scenes/Escape Room/Scripts/TrashPuzzle.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashPuzzle : MonoBehaviour
+{
+    public int trashItems = 5;
+    public GameObject door;
+    private Collider _trashcanCollider;
+    private List <GameObject> _currentCollisions = new List <GameObject> ();
+    private bool _solved = false;
+    
+    void Start()
+    {
+        _trashcanCollider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        // Add the GameObject collided with to the list.
+        _currentCollisions.Add (col.gameObject);
+        if (!_solved && _currentCollisions.Count >= trashItems)
+        { 
+            ValidateSolved();   
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        // Remove the GameObject collided with from the list.
+        _currentCollisions.Remove (col.gameObject);
+    }
+
+    private void ValidateSolved()
+    {
+        var trashCount = 0;
+        foreach (GameObject gObject in _currentCollisions) {
+            if (gObject.CompareTag("Trash"))
+            {
+                trashCount++;
+            }
+        }
+        if (trashCount == trashItems)
+        {
+            _solved = true;
+            door.GetComponent<Animator>().Play("Opening");
+        }
+    }
+}
