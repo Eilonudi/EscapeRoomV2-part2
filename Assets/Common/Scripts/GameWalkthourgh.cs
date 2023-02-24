c4b61d1600c1f77009dfebbd4698610b5841f036
Eilonudi <eilonudi@gmail.com>
2023-02-24 16:30:37 +0200

    adding buttons for walkthroguh

--- Assets/Common/Scripts/GameWalkthourgh.cs
+++ Assets/Common/Scripts/GameWalkthourgh.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class GameWalkthourgh : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject walkthroughCanvas;
+    public Animator firstTaskAnimator;
+    public Animator secondTaskAnimator;
+    public Animator thirdTaskAnimator;
+    public Animator forthTaskAnimator;
+    
+    private int lastCompletedTaskNumber = 0;
+    
     void Start()
     {
+        if (Application.isEditor)
+        {
+            print("We are running this from inside of the editor!");
+            walkthroughCanvas.SetActive(true);
+        }
+        else
+        {
+            walkthroughCanvas.SetActive(false);
+        }
         
+        // stop all animations
+        firstTaskAnimator.StopPlayback();
+        secondTaskAnimator.StopPlayback();
+        thirdTaskAnimator.StopPlayback();
+        forthTaskAnimator.StopPlayback();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartTaskAnimation()
     {
+        walkthroughCanvas.SetActive(false);
+        switch (lastCompletedTaskNumber)
+        {
+            case 0:
+                firstTaskAnimator.StartPlayback();
+                // disable first button
+                break;
+            case 2: 
+                secondTaskAnimator.StartPlayback();
+                // disable second button
+                break;
+            case 3: 
+                thirdTaskAnimator.StartPlayback();
+                // disable third button
+                break;
+            case 4: 
+                forthTaskAnimator.StartPlayback();
+                // disable forth button
+                break;
+        }
         
     }
 }
