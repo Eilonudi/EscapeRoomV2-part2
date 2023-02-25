4cf3eb96e080ecd6ae43df639dd4bf029d7359bb
Eilonudi <eilonudi@gmail.com>
2023-02-25 15:34:55 +0200

    disabling buttons between tasks

--- Assets/Common/Scripts/GameWalkthourgh.cs
+++ Assets/Common/Scripts/GameWalkthourgh.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameWalkthourgh : MonoBehaviour
 {
     public GameObject walkthroughCanvas;
     public GameObject xrRig;
     public GameObject trash;
-    
-    private int lastCompletedTaskNumber = 0;
-    
+    public Button firstTaskBtn;
+    public Button secondTaskBtn;
+    public Button thridTaskBtn;
+    public Button forthTaskBtn;
     void Start()
     {
         if (Application.isEditor)
         {
             print("We are running this from inside of the editor!");
             walkthroughCanvas.SetActive(true);
+            InitializeBtnListiners();
         }
         else
         {
@@ -42,5 +45,26 @@ public class GameWalkthourgh : MonoBehaviour
     private void ThrowToTrash()
     {
         trash.GetComponent<Animator>().Play("TrashToGarbage");
+    }
+
+    private void InitializeBtnListiners()
+    {
+        firstTaskBtn.onClick.AddListener(() =>
+        {
+            firstTaskBtn.interactable = false;
+            secondTaskBtn.interactable = true;
+        });
+        
+        secondTaskBtn.onClick.AddListener(() =>
+        {
+            secondTaskBtn.interactable = false;
+            thridTaskBtn.interactable = true;
+        });
+        
+        thridTaskBtn.onClick.AddListener(() =>
+        {
+            thridTaskBtn.interactable = false;
+            forthTaskBtn.interactable = true;
+        });
     }
 }
